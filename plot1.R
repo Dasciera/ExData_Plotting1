2ea033b38f24e01a6e79d57ac02e50b970c4106b
Dasciera <Dasciera.gmail.com>
2015-01-06 22:47:46 -0800

    Four png plots and four R scripts for Course Project 1

---
+++ plot1.R
@@ -0,0 +1,6 @@
+data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", quote = "", na.strings = "?")
+data2days <- subset(data, data$Date == "1/2/2007" | data$Date == "2/2/2007")
+par(bg = "transparent")
+hist(data2days$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.copy(png, file = "plot1.png", width = 480, height = 480)
+dev.off()
